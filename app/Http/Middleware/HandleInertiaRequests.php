0c19421b6e8fa9a4b4e85ca5a4dbb323a390eb43
chipslays <gorshkov34@gmail.com>
2026-01-28 11:09:51 +0400

    Update share method return type and user function
    Change return type of share method to array and update user function return type.

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,12 +33,12 @@ class HandleInertiaRequests extends Middleware
      *
      * @return array<string, mixed>
      */
-    public function share(Request $request): ?array
+    public function share(Request $request): array
     {
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => function () use ($request): array {
+                'user' => function () use ($request): ?array {
                     if (!$request->user()) {
                         return null;
                     }
